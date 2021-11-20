f549ebf700df0151c384e79cdd78cb8290856a77
Henrique Fuchs <65316438+henriquefuchs@users.noreply.github.com>
2021-11-20 15:26:18 -0300

    Corrige perfil da classe de teste

--- src/test/java/br/com/henriquefuchs/livraria/LivrariaApiApplicationTests.java
+++ src/test/java/br/com/henriquefuchs/livraria/LivrariaApiApplicationTests.java
@@ -2,8 +2,10 @@ package br.com.henriquefuchs.livraria;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class LivrariaApiApplicationTests {
 
   @Test
